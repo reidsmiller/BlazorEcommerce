857c37a2f198fd83b57f84b300ea7b434e0e8d10
reidsmiller <reid.s.miller@gmail.com>
2024-05-13 18:11:10 -0600

    add GetProductAsync to server service and controller

--- Server/Services/ProductService/ProductService.cs
+++ Server/Services/ProductService/ProductService.cs
@@ -18,5 +18,21 @@
 
             return response;
         }
+
+        public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
+        {
+            var response = new ServiceResponse<Product>();
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null)
+            {
+                response.Success = false;
+                response.Message = "Product not found.";
+            }
+            else
+            {
+                response.Data = product;
+            }
+            return response;
+        }
     }
 }
