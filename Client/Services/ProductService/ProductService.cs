da07d498e4781e82e2bcf463817d664e5a0b248a
reidsmiller <reid.s.miller@gmail.com>
2024-05-16 12:22:34 -0600

    add product search methods and suggestions to client

--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -8,14 +8,15 @@ namespace BlazorEcommerce.Client.Services.ProductService
     {
         private readonly HttpClient _http;
 
-        public event Action ProductsChanged;
-
         public ProductService(HttpClient http)
         {
             _http = http;
         }
 
         public List<Product> Products { get; set; } = new List<Product>();
+        public string Message { get; set; } = "Loading products..."
+
+        public event Action ProductsChanged;
 
         public async Task GetProducts(string? categoryUrl = null)
         {
@@ -31,6 +32,21 @@ namespace BlazorEcommerce.Client.Services.ProductService
         {
             var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
             return result;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+            if (result != null && result.Data != null) Products = result.Data;
+            if (Products.Count == 0) Message = "No products found.";
+
+            ProductsChanged?.Invoke();
+        }
+
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+            return result.Data;
         }
     }
 }
