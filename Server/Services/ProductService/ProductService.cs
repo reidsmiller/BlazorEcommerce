98ac18c5a63cb87d52c3e93ab238facc35e5b287
reidsmiller <reid.s.miller@gmail.com>
2024-05-15 18:25:38 -0600

    add variants and product types to GetProduct on server

--- Server/Services/ProductService/ProductService.cs
+++ Server/Services/ProductService/ProductService.cs
@@ -22,7 +22,10 @@
         public async Task<ServiceResponse<Product>> GetProduct(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
             if (product == null)
             {
                 response.Success = false;
