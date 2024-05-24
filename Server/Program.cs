8a7ded1c10b143a9d8e6b9b1caa2befb94fc38aa
reidsmiller <reid.s.miller@gmail.com>
2024-05-24 09:29:01 -0600

    add AuthServices with create and save password hash and salt

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,6 +5,8 @@ global using BlazorEcommerce.Server.Data;
 global using BlazorEcommerce.Server.Services.ProductService;
 global using BlazorEcommerce.Server.Services.CategoryService;
 global using BlazorEcommerce.Server.Services.CartService;
+global using BlazorEcommerce.Server.Services.AuthService;
+global using System.Security.Cryptography;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +26,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 
 var app = builder.Build();
