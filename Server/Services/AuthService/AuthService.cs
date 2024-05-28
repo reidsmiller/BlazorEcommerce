32312f4217ebe02b05a408721a789986a25d8a3c
reidsmiller <reid.s.miller@gmail.com>
2024-05-28 14:30:52 -0600

    add login page and methods

--- Server/Services/AuthService/AuthService.cs
+++ Server/Services/AuthService/AuthService.cs
@@ -10,6 +10,30 @@ namespace BlazorEcommerce.Server.Services.AuthService
             _context = context;
         }
 
+        public async Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            var response = new ServiceResponse<string>();
+            var user = await _context.Users.FirstOrDefaultAsync(user => user.Email.ToLower().Equals(email.ToLower()));
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "Authentication information incorrect.";
+            }
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Success = false;
+                response.Message = "Authentication information incorrect.";
+            }
+            else
+            {
+                response.Data = "token";
+            }
+
+            response.Data = "token";
+
+            return response;
+        }
+
         public async Task<ServiceResponse<int>> Register(User user, string password)
         {
             if (await UserExists(user.Email))
@@ -43,6 +67,15 @@ namespace BlazorEcommerce.Server.Services.AuthService
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
             }
         }
     }
