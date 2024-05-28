482bcdb0fedefe789555dd6e1fe0f3c5c0819fa2
reidsmiller <reid.s.miller@gmail.com>
2024-05-28 13:25:49 -0600

    add client side registration server request and response

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,4 @@
+global using BlazorEcommerce.Client.Services.AuthService;
 global using BlazorEcommerce.Client.Services.ProductService;
 global using BlazorEcommerce.Client.Services.CategoryService;
 global using BlazorEcommerce.Client.Services.CartService;
@@ -18,5 +19,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 await builder.Build().RunAsync();
